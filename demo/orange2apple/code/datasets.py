27e6044670164e8477d0a671f868c5c8ecc56ac1
Hao Luo <1091627587@qq.com>
2024-10-24 18:20:00 +0800

    Add files via upload

---
+++ demo/orange2apple/code/datasets.py
@@ -0,0 +1,63 @@
+import glob
+import random
+import os
+import torchvision.transforms as transforms
+
+
+from matplotlib import pyplot as plt
+from torch.utils.data import Dataset
+from PIL import Image
+
+
+def to_rgb(image):
+    rgb_image = Image.new("RGB", image.size)
+    rgb_image.paste(image)
+    return rgb_image
+
+
+class ImageDataset(Dataset):
+    def __init__(self, root, transforms_=None, unaligned=False, mode="train"):
+        self.transform = transforms.Compose(transforms_)
+        self.unaligned = unaligned
+
+        self.files_A = sorted(glob.glob(os.path.join(root, "%sA" % mode) + "/*.*"))
+        self.files_B = sorted(glob.glob(os.path.join(root, "%sB" % mode) + "/*.*"))
+
+    def __getitem__(self, index):
+        image_A = Image.open(self.files_A[index % len(self.files_A)])
+
+        if self.unaligned:
+            image_B = Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)])
+        else:
+            image_B = Image.open(self.files_B[index % len(self.files_B)])
+
+        # Convert grayscale images to rgb
+        if image_A.mode != "RGB":
+            image_A = to_rgb(image_A)
+        if image_B.mode != "RGB":
+            image_B = to_rgb(image_B)
+
+        item_A = self.transform(image_A)
+        item_B = self.transform(image_B)
+        return {"A": item_A, "B": item_B}
+
+    def __len__(self):
+        return max(len(self.files_A), len(self.files_B))
+
+
+if __name__ == "__main__":
+    img_height, img_width = 256, 256
+    transforms_ = [
+        transforms.Resize(int(img_height * 1.12), Image.BICUBIC),
+        transforms.RandomCrop((img_height, img_width)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+
+    dataset = ImageDataset("./images/apple2orange", transforms_=transforms_, unaligned=True)
+    img = dataset[20]['A'].numpy().transpose(2, 1, 0)
+    plt.imshow(img)
+    plt.show()
+
+
